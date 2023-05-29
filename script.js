05b40c97b66c175dc4b48c04728a3a3a0e805658
phillip-steven <putu.phillip@gmail.com>
2023-05-30 05:00:01 +0700

    Update javascript

--- script.js
+++ script.js
@@ -2,20 +2,60 @@
 const date = new Date();
 document.getElementById(
   "todayDate"
-).innerHTML = `${date.getDate()} /${date.getMonth()} / ${date.getFullYear()}`;
+).value = `${date.getDate()} /${date.getMonth()} / ${date.getFullYear()}`;
 // End of define date
 
+// Start calculate outtake function
 function calculateOuttake(constWeight, todayWeight, magicNumber) {
   return parseInt(todayWeight * 1000 - constWeight * 1000 + magicNumber);
 }
+// End calculate outtake function
+// Set outtake to 0
+document.getElementById("outTake").innerText = `0`;
 
-function submitOuttake() {
-  const cWeight = Number(document.getElementById("constantWeight").value);
-  const tWeight = Number(document.getElementById("todayWeight").value);
-  const mNumber = Number(document.getElementById("magicNumber").value);
-  return (document.getElementById("outTake").innerText = ` ${calculateOuttake(
+// Start function update outtake
+function updateOuttake() {
+  let cWeight = Number(document.getElementById("constantWeight").value);
+  let tWeight = Number(document.getElementById("todayWeight").value);
+  let mNumber = Number(document.getElementById("magicNumber").value);
+  return (document.getElementById("outTake").value = ` ${calculateOuttake(
     cWeight,
     tWeight,
     mNumber
   )}`);
+}
+// End function update outtake
+
+// Set update display to reset input value
+function updateDisplay() {
+  let cWeight = (document.getElementById("constantWeight").value = "");
+  let tWeight = (document.getElementById("todayWeight").value = "");
+  let mNumber = (document.getElementById("magicNumber").value = "");
+}
+
+function formValidation() {
+  let cWeight = (document.getElementById("constantWeight").value = "");
+  let tWeight = (document.getElementById("todayWeight").value = "");
+  let mNumber = (document.getElementById("magicNumber").value = "");
+  if (
+    isNaN(cWeight) ||
+    cWeight < 20 ||
+    (cWeight > 150 && isNaN(tWeight)) ||
+    tWeight < 20 ||
+    (tWeight > 150 && isNaN(mNumber)) ||
+    mNumber < 20 ||
+    mNumber > 150
+  ) {
+    document.getElementById("outTake").innerText = `Input tidak valid`;
+    return false;
+  } else {
+    updateOuttake();
+  }
+}
+console.log(formValidation);
+// Submit outtake
+function submitOuttake() {
+  // formValidation();
+  updateOuttake();
+  // updateDisplay();
 }
